266005e0ef25ec0670a8e09240deae16c1488a0d
Matt Newport <matt@mattnewport.com>
2015-10-23 16:58:10 -0700

    Added support for inserting an object with a type that is not statically known, providing a bucket already exists for it to live in.

--- EntityBag/main.cpp
+++ EntityBag/main.cpp
@@ -1,5 +1,7 @@
+#include <cassert>
 #include <iostream>
 #include <memory>
+#include <string>
 #include <tuple>
 #include <typeindex>
 #include <unordered_map>
@@ -12,6 +14,7 @@ class EntityBag {
     struct TypeBagBase {
         TypeBagBase(size_t entitySize_) : entitySize{entitySize_} {}
         virtual ~TypeBagBase() {}
+        virtual void emplace(const EntityBase& x) = 0;
         auto size() { return numElems; }
         EntityBase* entity(size_t index) {
             return reinterpret_cast<EntityBase*>(baseElems + index * entitySize);
@@ -25,6 +28,7 @@ class EntityBag {
     template <typename T>
     struct TypeBag : public TypeBagBase {
         TypeBag() : TypeBagBase{sizeof(T)} {}
+        virtual void emplace(const EntityBase& x) { emplace(static_cast<const T&>(x)); }
         template <typename... Args>
         void emplace(Args&&... args) {
             elems.emplace_back(std::forward<Args>(args)...);
@@ -45,9 +49,7 @@ public:
                    std::tie(x.currentTypeBag, x.currentElementIndex);
         }
         bool operator!=(const iterator& x) { return !(*this == x); }
-        EntityBase* operator*() {
-            return currentTypeBag->second->entity(currentElementIndex);
-        }
+        EntityBase* operator*() { return currentTypeBag->second->entity(currentElementIndex); }
         iterator& operator++() {
             if (currentElementIndex < currentTypeBag->second->size() - 1) {
                 ++currentElementIndex;
@@ -69,6 +71,20 @@ public:
         if (!typeBagBaseUp) typeBagBaseUp = std::make_unique<TypeBag<T>>();
         auto& typeBag = static_cast<TypeBag<T>&>(*typeBagBaseUp);
         typeBag.emplace(args...);
+    }
+
+    // Insert a copy of x with concrete type T dynamically worked out using RTTI, this requires a
+    // TypeBag<T> already exists in the map since in general we don't have a way to create one
+    // otherwise.
+    void emplace(const EntityBase& x) {
+        auto findIt = typeBags.find(typeid(x));
+        if (findIt != typeBags.end()) {
+            findIt->second->emplace(x);
+        } else {
+            auto msg =
+                std::string{"Could not find an existing type bag for type "} + typeid(x).name();
+            assert(false && msg.c_str());
+        }
     }
 
     iterator begin() const { return iterator{typeBags.begin()}; }
@@ -114,6 +130,9 @@ int main() {
     entityBag.emplace<Baz>(2.0f);
     entityBag.emplace<Bar>(4, 6.0f);
     entityBag.emplace<Baz>(3.0f);
+    Baz aBaz{4.0f};
+    Foo& aFooRef = aBaz;
+    entityBag.emplace(aFooRef);
     for (auto e : entityBag) {
         e->update();
     }
